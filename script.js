a808588336d1c7a559c877b99782328888bdcee5
spahicadev <aspahic@student.unipu.hr>
2024-10-09 13:06:49 +0200

    Everythin else implemente like randomColors, canvas mode. Now start fixing errors, like canvas mode. And do some refactoring if its necessary

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const canvasMode = document.querySelectorAll('.etch-a-sketch__canvas-mode-button
 const resetBtn = document.querySelector('.etch-a-sketch__control-box-button');
 
 
-//napraviti jos randomBoje
+
+
 
 //STATEs
 let measurmentsValue = areaMeasurments.value;
@@ -14,7 +15,11 @@ const initalValueCopy = measurmentsValue;
 let smallMode = false;
 let mediumMode = true;
 let largeMode = false;
+const randomNumber = () => Math.floor(Math.random() * 255);
 
+
+
+//BINDING
 const defaultArea = () => {
   canvasMode[1].disabled = true;
   choosenMeasurments.textContent = `${measurmentsValue}x${measurmentsValue}`
@@ -28,7 +33,7 @@ const defaultArea = () => {
   const generatedBoxes = document.querySelectorAll('.etch-a-sketch__canvas-canvas-box-item');
   generatedBoxes.forEach((box) => {
     box.addEventListener('mouseover', (e) => {
-      e.target.classList.add('box')
+      e.target.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
       resetBtn.addEventListener('click', () => {
         box.classList.remove('box');
 
@@ -71,7 +76,7 @@ areaMeasurments.addEventListener('input', (e) => {
   const generatedBoxes = document.querySelectorAll('.etch-a-sketch__canvas-canvas-box-item');
   generatedBoxes.forEach((box) => {
     box.addEventListener('mouseover', (e) => {
-      e.target.classList.add('box');
+      e.target.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
       resetBtn.addEventListener('click', () => {
         box.classList.remove('box');
         canvasMode[1].disabled = true;
@@ -89,6 +94,95 @@ areaMeasurments.addEventListener('input', (e) => {
       })
     })
   }); 
+})
+
+
+canvasMode.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+   if(e.target.dataset.mode === 'small') {
+    e.target.disabled = true;
+    canvasMode[1].disabled = false;
+    canvasMode[2].disabled = false;
+    const classes = canvasArea.classList
+    let temp = [];
+    classes.forEach((value) => {
+      if(value !== 'etch-a-sketch__canvas-canvas-box') {
+        temp.push(value)
+      }
+    temp.forEach((nameOfClass) => {
+      classes.remove(nameOfClass);
+    })
+    });
+    canvasArea.innerHTML = '';
+    canvasArea.classList.add('etch-a-sketch__canvas-canvas-box--mode-small');
+    const measures = getComputedStyle(canvasArea);
+    const columns = measures.getPropertyValue('grid-template-columns');
+    const rows = measures.getPropertyValue('grid-template-rows');
+    for(let i = 0; i<(Number(columns.split(' ').length) * Number(rows.split(' ').length)); i++) { //proci, razmislit
+          const minBox = document.createElement("div");
+          minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
+          canvasArea.appendChild(minBox)
+
+          
+        }
+    
+
+   } else if(e.target.dataset.mode === 'medium') {
+    e.target.disabled = true;
+    canvasMode[0].disabled = false;
+    canvasMode[2].disabled = false;
+    const classes = canvasArea.classList
+    let temp = [];
+    classes.forEach((value) => {
+      if(value !== 'etch-a-sketch__canvas-canvas-box') {
+        temp.push(value)
+      }
+    temp.forEach((nameOfClass) => {
+      classes.remove(nameOfClass);
+    })
+    });
+    canvasArea.innerHTML = '';
+    canvasArea.classList.add('etch-a-sketch__canvas-canvas-box--mode-medium');
+    const measures = getComputedStyle(canvasArea);
+    const columns = measures.getPropertyValue('grid-template-columns');
+    const rows = measures.getPropertyValue('grid-template-rows');
+    for(let i = 0; i<(Number(columns.split(' ').length) * Number(rows.split(' ').length)); i++) { //proci, razmislit
+          const minBox = document.createElement("div");
+          minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
+          canvasArea.appendChild(minBox)
+
+          
+        }
+
+   } 
+    else if(e.target.dataset.mode === 'large') {
+    e.target.disabled = true;
+    canvasMode[0].disabled = false;
+    canvasMode[1].disabled = false;
+    const classes = canvasArea.classList
+    let temp = [];
+    classes.forEach((value) => {
+      if(value !== 'etch-a-sketch__canvas-canvas-box') {
+        temp.push(value)
+      }
+    temp.forEach((nameOfClass) => {
+      classes.remove(nameOfClass);
+    })
+    });
+    canvasArea.innerHTML = '';
+    canvasArea.classList.add('etch-a-sketch__canvas-canvas-box--mode-large');
+    const measures = getComputedStyle(canvasArea);
+    const columns = measures.getPropertyValue('grid-template-columns');
+    const rows = measures.getPropertyValue('grid-template-rows'); 
+    for(let i = 0; i<(Number(columns.split(' ').length) * Number(rows.split(' ').length)); i++) { //proci, razmislit
+          const minBox = document.createElement("div");
+          minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
+          canvasArea.appendChild(minBox)
+          
+        } 
+    
+   }
+  })
 })
 
 
