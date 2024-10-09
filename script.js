cba0e2f89d2e8a61d89870cdfb4e82732589fba4
spahicadev <aspahic@student.unipu.hr>
2024-10-09 10:58:18 +0200

    Canvas area done, now start with mode buttons and random colors for each box

--- script.js
+++ script.js
@@ -1,24 +1,96 @@
 //DOM
-const canvasArea = document.querySelector('.etch-a-sketch__canvas-canvas-boxes');
+const canvasArea = document.querySelector('.etch-a-sketch__canvas-canvas-box');
 const areaMeasurments = document.querySelector('.etch-a-sketch__control-box-track');
 const choosenMeasurments = document.querySelector('.etch-a-sketch__canvas-control-box-measures');
+const canvasMode = document.querySelectorAll('.etch-a-sketch__canvas-mode-button');
+const resetBtn = document.querySelector('.etch-a-sketch__control-box-button');
 
 
-areaMeasurments.addEventListener('input', (e) => {
-  choosenMeasurments.textContent = `${e.target.value}x${e.target.value}`
-  canvasArea.style.gridTemplateColumns = `repeat(${e.target.value}, 1fr)`
-  canvasArea.style.gridTemplateRows = `repeat(${e.target.value}, 1fr)`
-  canvasArea.innerHTML = '' //proci, razmislit
-  for(let i = 0; i<(Number(e.target.value) * Number(e.target.value)); i++) { //proci, razmislit
+//napraviti jos randomBoje
+
+//STATEs
+let measurmentsValue = areaMeasurments.value;
+const initalValueCopy = measurmentsValue;
+let smallMode = false;
+let mediumMode = true;
+let largeMode = false;
+
+const defaultArea = () => {
+  canvasMode[1].disabled = true;
+  choosenMeasurments.textContent = `${measurmentsValue}x${measurmentsValue}`
+  canvasArea.style.gridTemplateColumns = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+  canvasArea.style.gridTemplateRows = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+  for(let i = 0; i<(Number(measurmentsValue) * Number(measurmentsValue)); i++) { //proci, razmislit
     const minBox = document.createElement("div");
-    minBox.classList.add('grid--item')
+    minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
     canvasArea.appendChild(minBox)
   }
-  const generatedBoxes = document.querySelectorAll('.grid--item');
+  const generatedBoxes = document.querySelectorAll('.etch-a-sketch__canvas-canvas-box-item');
   generatedBoxes.forEach((box) => {
     box.addEventListener('mouseover', (e) => {
-      e.target.style.background = 'blue'
-      
+      e.target.classList.add('box')
+      resetBtn.addEventListener('click', () => {
+        box.classList.remove('box');
+
+      })
     })
-  })
+  }); 
+}
+
+defaultArea()
+
+areaMeasurments.addEventListener('input', (e) => {
+  canvasMode[1].disabled = false
+  measurmentsValue = e.target.value;
+  choosenMeasurments.textContent = `${measurmentsValue}x${measurmentsValue}`;
+  canvasArea.style.gridTemplateColumns = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+  canvasArea.style.gridTemplateRows = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+  canvasArea.innerHTML = ''
+  for(let i = 0; i<(Number(measurmentsValue) * Number(measurmentsValue)); i++) { //proci, razmislit
+    const minBox = document.createElement("div");
+    minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
+    canvasArea.appendChild(minBox)
+  }
+
+  resetBtn.addEventListener('click', () => {
+    canvasMode[1].disabled = true;
+    measurmentsValue = initalValueCopy;
+    areaMeasurments.value = measurmentsValue;
+    choosenMeasurments.textContent = `${measurmentsValue}x${measurmentsValue}`;
+    canvasArea.style.gridTemplateColumns = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+    canvasArea.style.gridTemplateRows = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+    canvasArea.innerHTML = ''
+    for(let i = 0; i<(Number(measurmentsValue) * Number(measurmentsValue)); i++) { //proci, razmislit
+      const minBox = document.createElement("div");
+      minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
+      canvasArea.appendChild(minBox)
+    }
+  });
+
+  
+  const generatedBoxes = document.querySelectorAll('.etch-a-sketch__canvas-canvas-box-item');
+  generatedBoxes.forEach((box) => {
+    box.addEventListener('mouseover', (e) => {
+      e.target.classList.add('box');
+      resetBtn.addEventListener('click', () => {
+        box.classList.remove('box');
+        canvasMode[1].disabled = true;
+        measurmentsValue = initalValueCopy;
+        areaMeasurments.value = measurmentsValue;
+        choosenMeasurments.textContent = `${measurmentsValue}x${measurmentsValue}`;
+        canvasArea.style.gridTemplateColumns = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+        canvasArea.style.gridTemplateRows = `repeat(${measurmentsValue}, 1fr)`//razumjeti
+        canvasArea.innerHTML = ''
+        for(let i = 0; i<(Number(measurmentsValue) * Number(measurmentsValue)); i++) { //proci, razmislit
+          const minBox = document.createElement("div");
+          minBox.classList.add('etch-a-sketch__canvas-canvas-box-item')
+          canvasArea.appendChild(minBox)
+        }
+      })
+    })
+  }); 
 })
+
+
+
+
